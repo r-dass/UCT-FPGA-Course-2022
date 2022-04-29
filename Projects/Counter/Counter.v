973243dc78f609a51f03aa78c94e680ea46233b3
Reev <reevelendass@gmail.com>
2022-04-29 13:01:20 +0200

    Removed UART Hardware Project. Prac 1-2 Complete + UART Sim

--- Projects/Counter/Counter.v
+++ Projects/Counter/Counter.v
@@ -7,8 +7,9 @@ module Counter(
 reg [30:0]Count = 0;
 
 always @(posedge ipClk) begin
-	if (!ipReset) Count <= 0;
-	else Count++;
+	if (ipReset) Count <= 0;
+	else 
+		Count++;
 end
 	
 assign opLED = Count[30:23];
--- Projects/UART_Hardware/impl1/synlog.tcl
+++
@@ -1,1 +0,0 @@
-run_tcl -fg UART_Hardware_impl1_synplify.tcl
