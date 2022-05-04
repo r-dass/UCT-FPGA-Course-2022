b54f2d4f61e30a8c1871ae306f58d61e499ea445
Reev <reevelendass@gmail.com>
2022-05-04 16:45:15 +0200

    More Setup for Controller File

--- Projects/Counter/Controller.v
+++ Projects/Counter/Controller.v
@@ -1,17 +1,29 @@
 import Structures::*;
 //------------------------------------------------------------------------------
 module Control(
-    input           ipClk,
-    input           ipReset,
+    input               ipClk,
+    input               ipReset,
 
-    input           ipRxStream,
-    output          opTxStream,
+    input   UART_PACKET ipRxStream,
+    output  UART_PACKET opTxStream,
 
-    input reg[31:0] ipRdData
-    output [7:0]    ipAddress,
-    output[31:0]    ipWrData,
-    output          ipWrEnable, 
+    input   reg[31:0]   ipRdData
+    output  [7:0]       ipAddress,
+    output  [31:0]      ipWrData,
+    output              ipWrEnable, 
 );
+
+always @ (posedge(ipClk)) begin
+  if(ipRxStream.Valid) begin
+      if (ipRxStream.ipAddress == 0) begin
+          // write
+      end  
+
+      if (ipRxStream.ipAddress == 1) begin
+          // read
+      end
+  end
+end
 
 endmodule
 //------------------------------------------------------------------------------
