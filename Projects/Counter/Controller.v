ee675f37669f5fa3167b299f9847027680f87f11
Reev <reevelendass@gmail.com>
2022-07-02 14:57:18 +0200

    Controller Write Draft

--- Projects/Counter/Controller.v
+++ Projects/Counter/Controller.v
@@ -4,15 +4,16 @@ module Control(
     input               ipClk,
     input               ipReset,
 
+    output  UART_PACKET opTxStream,
     input				ipTxReady,
+
+	output  [7:0]       opAddress,
+    output  [31:0]      opWrData,
+    output              opWrEnable
 	
 	input   UART_PACKET ipRxStream,
-    output  UART_PACKET opTxStream,
 
     input   reg[31:0]   ipRdData,
-    output  [7:0]       opAddress,
-    output  [31:0]      opWrData,
-    output               opWrEnable
 );
 
 enum {
@@ -21,10 +22,19 @@ enum {
 	Write
 } State;
 
+enum {
+	WriteAddress,
+	WriteData
+} wState;
+
+reg [1:0] Count;
+
 always @ (posedge(ipClk)) begin
 	if (!ipReset) begin
 		case (State)
 			Wait: begin
+				opWrEnable <= 1;
+
 				if(ipRxStream.Valid && ipRxStream.SoP) begin
 					if (ipRxStream.Destination == 0) begin
 						State <= Read;
@@ -35,7 +45,25 @@ always @ (posedge(ipClk)) begin
 				end
 			end
 			Write: begin
+				case (wState)
+					WriteAddress: begin
+						if (ipRxStream.Valid) begin
+							opAddress <= ipRxStream.Data;
+							WState <= WriteData;
+						end		
+					end
+					WriteData: begin
+						if (ipRxStream.Valid) begin
+							opWrData <= {ipRxStream.Data, opWrData[31:8]};
+							Count <= Count + 1;
 
+							if (Count == 3) begin
+								WState <= WriteAddress;
+								State <= Wait;
+								opWrEnable <= 1;
+							end
+						end
+					end
 			end 
 			Read: begin
 
@@ -43,7 +71,7 @@ always @ (posedge(ipClk)) begin
 		 	default:;	
 		endcase
 	end else begin // reset code here
-		
+		Count <= 0;
 	end
 end
 
