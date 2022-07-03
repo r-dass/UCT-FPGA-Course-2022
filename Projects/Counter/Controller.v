350151f2a86b8525f9d8b1331249a68bf0e8417b
Reev <reevelendass@gmail.com>
2022-07-03 22:54:33 +0200

    Updated Controller Name

--- Projects/Counter/Controller.v
+++ Projects/Counter/Controller.v
@@ -1,6 +1,6 @@
 import Structures::*;
 //------------------------------------------------------------------------------
-module Control(
+module Controller(
     input               ipClk,
     input               ipReset,
 
